d47d3ab4a3f52c8d028054304bae741674bdfa06
Shital Khaire <shitalkhaire.mail@gmail.com>
2023-10-01 14:40:23 +0200

    commit for WritingOutPutToFile

---
+++ Scripts/WritingOutputToFile.ps1
@@ -0,0 +1,36 @@
+
+# Used param() for passing multiple arguments.
+param(
+  [string]$userName,
+  [int]$userId,
+  [string]$ProductName,
+  [int]$ProductPrice
+)
+<# instead of displaying output on screen just written in userinfo.txt file.
+write-output "User name is $userName"
+write-output "User id is $userId"
+write-output "Product name is $ProductName"
+write-output "Product Price is $ProductPrice"
+#>
+
+$output = @($userName,$userId,$ProductName,$ProductPrice)
+#Write-Output "---------------------------------------------------------$output"
+
+$output | Add-Content -Path D:\Powershell\Projects\Sample\userinfo.txt
+Write-Output "File Information is here.."
+$filesRecords = Get-Content -path D:\Powershell\Projects\Sample\userinfo.txt
+$lines = 0
+$productCount = 0
+
+$search = Read-Host -Prompt "ENter product name for search:"
+
+foreach($record in $filesRecords) 
+{
+
+  if($record -match $search)  # The term in brackets is a true/false value
+  {
+    $productCount+=1; 
+  }
+  #Write-Output "similar products count is := $productCount "
+}
+Write-Output $search "purchased" $productCount "times"
